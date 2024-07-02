4249585754cd58735359486721026f40aae63566
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-07-03 00:05:00 +0300

    Done with day 1

--- weather-app/src/components/Plate.jsx
+++ weather-app/src/components/Plate.jsx
@@ -1,11 +1,60 @@
 import Search from "./Search";
 import styles from './Plate.module.css'
+import { useEffect, useState } from "react";
 
 
 
 export default function Plate() {
+    const [city,setCity] = useState('')
+    const [loading,setLoading] = useState(false) ; 
+    const [weatherData,setWeatherdata] = useState(null)
+
+    const APIKEY = "ce84fb179b85e9494c8de9398646a1a6"
+
+    async function fetchWeatherdata(param) {
+        setLoading(true)
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${APIKEY}`)
+
+            const data = await response.json();
+            console.log(data,"data");
+            if (data) {
+                setWeatherdata(data)
+                setLoading(false)
+            }
+        } catch (error) {
+            setLoading(false)
+            console.log(error);
+        }
+    }
+    
+
+    async function handleClick() {
+        fetchWeatherdata(city);
+    }
+
+    console.log(loading);
+    useEffect(()=>{
+        fetchWeatherdata("nairobi")
+        console.log(weatherData);
+    },[])
     return <div className={styles.plate} >
-            <Search/>
+            <Search  city={city}
+            setCity={setCity} 
+            handleClick={handleClick}
+            />
+
+            {
+                loading ? (<div>Loading...</div>)  : 
+                 (<div>
+                    <div className="city-name" >
+                        <h4>
+                            {weatherData?.name} , <span> {weatherData?.sys?.country} </span>
+                        </h4>
+                    </div>
+                 </div>)
+            }
             
+
     </div>
 }
--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react"
 import styles from './Search.module.css'
 
-export default function Search() {
+export default function Search({city,setCity,handleClick}) {
     
-    const [city,setCity] = useState('')
+   
 
     return <div>
+
+
 
         <input className={styles.input} type="text" placeholder="Enter your city" 
         value={city}
         onChange={(e)=>setCity(e.target.value)}
          />
 
-         <button className={styles.button} >
+         <button onClick={handleClick} className={styles.button} >
             Search
          </button>
 
